0a2881345d7f34c2afaacbef5ab8b589d8699468
赵波 <792598794@qq.com>
2018-12-28 15:37:10 +0800

    no message

--- ui/templates.go
+++ ui/templates.go
@@ -7,13 +7,13 @@ import (
 )
 
 var (
-	TplLayout = template.Must(template.New("layout.html").ParseFiles("ui/templates/layout.html"))
+	TplLayout = template.Must(template.New("layout.html").ParseFiles("./ui/templates/layout.html"))
 
-	TplBalancerList     = template.Must(template.Must(TplLayout.Clone()).ParseFiles("ui/templates/balancerList.html"))
-	TplBalancerNewForm  = template.Must(template.Must(TplLayout.Clone()).ParseFiles("ui/templates/balancerNewForm.html"))
-	TplBalancerView     = template.Must(template.Must(TplLayout.Clone()).ParseFiles("ui/templates/balancerView.html"))
-	TplBalancerEditForm = template.Must(template.Must(TplLayout.Clone()).ParseFiles("ui/templates/balancerEditForm.html"))
+	TplBalancerList     = template.Must(template.Must(TplLayout.Clone()).ParseFiles("./ui/templates/balancerList.html"))
+	TplBalancerNewForm  = template.Must(template.Must(TplLayout.Clone()).ParseFiles("./ui/templates/balancerNewForm.html"))
+	TplBalancerView     = template.Must(template.Must(TplLayout.Clone()).ParseFiles("./ui/templates/balancerView.html"))
+	TplBalancerEditForm = template.Must(template.Must(TplLayout.Clone()).ParseFiles("./ui/templates/balancerEditForm.html"))
 
-	TplServerNewForm  = template.Must(template.Must(TplLayout.Clone()).ParseFiles("ui/templates/serverNewForm.html"))
-	TplServerEditForm = template.Must(template.Must(TplLayout.Clone()).ParseFiles("ui/templates/serverEditForm.html"))
+	TplServerNewForm  = template.Must(template.Must(TplLayout.Clone()).ParseFiles("./ui/templates/serverNewForm.html"))
+	TplServerEditForm = template.Must(template.Must(TplLayout.Clone()).ParseFiles("./ui/templates/serverEditForm.html"))
 )
