dd60fa3a422f55d138a393a38a10d7e7d816e57d
赵波 <792598794@qq.com>
2018-12-29 11:10:13 +0800

    no message

--- ui/templates.go
+++ ui/templates.go
@@ -3,17 +3,34 @@
 package ui
 
 import (
+	"fmt"
 	"html/template"
+	"os"
+	"os/exec"
+	"strings"
 )
 
+func GetCurrentPath() string {
+	s, err := exec.LookPath(os.Args[0])
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+	s = strings.Replace(s, "\\", "/", -1)
+	s = strings.Replace(s, "\\\\", "/", -1)
+	i := strings.LastIndex(s, "/")
+	path := string(s[0 : i+1])
+	return path
+}
+
 var (
-	TplLayout = template.Must(template.New("layout.html").ParseFiles("ui/templates/layout.html"))
 
-	TplBalancerList     = template.Must(template.Must(TplLayout.Clone()).ParseFiles("ui/templates/balancerList.html"))
-	TplBalancerNewForm  = template.Must(template.Must(TplLayout.Clone()).ParseFiles("ui/templates/balancerNewForm.html"))
-	TplBalancerView     = template.Must(template.Must(TplLayout.Clone()).ParseFiles("ui/templates/balancerView.html"))
-	TplBalancerEditForm = template.Must(template.Must(TplLayout.Clone()).ParseFiles("ui/templates/balancerEditForm.html"))
+	TplLayout = template.Must(template.New("layout.html").ParseFiles(GetCurrentPath() + "templates/layout.html"))
 
-	TplServerNewForm  = template.Must(template.Must(TplLayout.Clone()).ParseFiles("ui/templates/serverNewForm.html"))
-	TplServerEditForm = template.Must(template.Must(TplLayout.Clone()).ParseFiles("ui/templates/serverEditForm.html"))
+	TplBalancerList     = template.Must(template.Must(TplLayout.Clone()).ParseFiles(GetCurrentPath() + "templates/balancerList.html"))
+	TplBalancerNewForm  = template.Must(template.Must(TplLayout.Clone()).ParseFiles(GetCurrentPath() + "templates/balancerNewForm.html"))
+	TplBalancerView     = template.Must(template.Must(TplLayout.Clone()).ParseFiles(GetCurrentPath() + "templates/balancerView.html"))
+	TplBalancerEditForm = template.Must(template.Must(TplLayout.Clone()).ParseFiles(GetCurrentPath() + "templates/balancerEditForm.html"))
+
+	TplServerNewForm  = template.Must(template.Must(TplLayout.Clone()).ParseFiles(GetCurrentPath() + "templates/serverNewForm.html"))
+	TplServerEditForm = template.Must(template.Must(TplLayout.Clone()).ParseFiles(GetCurrentPath() + "templates/serverEditForm.html"))
 )
